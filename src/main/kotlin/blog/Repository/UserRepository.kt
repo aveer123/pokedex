4f0d46fae506c653806cb4414539fa29984c922f
Alec Veer <alec_veer@yahoo.com>
2021-08-03 11:30:58 -0500

    add userRepository to repo

--- src/main/kotlin/blog/Repository/UserRepository.kt
+++ src/main/kotlin/blog/Repository/UserRepository.kt
@@ -5,8 +5,7 @@ import blog.models.User
 import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
-import blog.Repository.UserRepository
-import org.springframework.web.servlet.HandlerExceptionResolver
+
 
 
 @Repository
